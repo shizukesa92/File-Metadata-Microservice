a941ab698981bd800e0223af468f9df2266f4df3
Leonard Tye <leonardtye1992@gmail.com>
2018-04-13 10:47:52 +0800

    Everything works!

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require("express");
-const route = require("./server/routes/router");
+const multer = require("multer");
 
 const app = express();
+const upload = multer();
 
-app.use("/", route);
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+	return (!req.file) ? res.end(JSON.stringify({
+		name: null,
+		size: null
+	})) : res.end(JSON.stringify({
+		name: req.file.originalname,
+		size: req.file.size
+	}));
+});
+
+app.use(express.static("./dist/client")); // Use dist and not client because server renders dist
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "./dist/client/index.html")); // Cannot use render for html unlike pug etc
+});
 
 app.listen(process.env.PORT || 3000);
